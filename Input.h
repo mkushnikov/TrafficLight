f65ddd9e98ad0c61a8e56e613091d9a1776bcd64
mkushnikov <kushnikov.mikhail@gmail.com>
2021-09-03 00:46:07 +0300

    Добавил комменты

--- Input.h
+++ Input.h
@@ -7,15 +7,21 @@
 class Input
 {
 private:
+    // Нажатая клавиша
     char pressedKey_;
+
+    // Список подписчиков
     std::vector<std::function<void(char pressedKey)>> subsList_;
 
 public:
     // Основная функция для ввода
     // Реагирует только на латинские буквы
     void handleInput();
-    
+
+    // Добавляем подписчиков в список
     void addSub(std::function<void(char pressedKey)> subCallback);
+
+    // Уведомляем всех подписчиков о событии
     void notifySubs();
 
     Input()
